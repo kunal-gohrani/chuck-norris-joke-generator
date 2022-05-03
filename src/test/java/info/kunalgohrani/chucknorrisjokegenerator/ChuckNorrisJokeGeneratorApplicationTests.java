2ae0a203507d1bd8561b68592e2d9b4d4b702d44
Kunal Gohrani <gohranikunal25@outlook.com>
2022-05-03 20:37:31 +0530

    Initial Commit

---
+++ src/test/java/info/kunalgohrani/chucknorrisjokegenerator/ChuckNorrisJokeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package info.kunalgohrani.chucknorrisjokegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
